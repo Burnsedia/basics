073348f188793aa2b740063de2ccb8147fcc5ddd
burnsedia <bburnsed1995@gmail.com>
2019-11-09 20:42:13 -0500

    made a char demo

---
+++ char.py
@@ -0,0 +1,18 @@
+#!/bin/python3.7
+
+# char is a single unicode charitor
+# user input is read by the OS as char
+# if you press the 1 key it is read as a char not an int
+
+a = "a"
+
+one = "1"
+
+# sinngle quotes and duoble quotes are interchangable in most 
+# places 
+
+a_am_also_a = 'a'
+
+I_am_also_one = '1'
+
+
