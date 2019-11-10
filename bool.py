3a651f2c9dae942aa6a38c50a3f52b09310cbc30
burnsedia <bburnsed1995@gmail.com>
2019-11-09 20:33:37 -0500

    made a demo for bools

---
+++ bool.py
@@ -0,0 +1,11 @@
+#!/bin/python3.7
+
+# Bools are true or false statmants
+# Bools are used when there are only 
+# two possible answers
+
+if True: 
+    print("this is true")
+
+if False:
+    print("this is false")
